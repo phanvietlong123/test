ede56385cb7e902fd1c2e856e5c339a994f1417e
phanvietlong123 <pvlong.18it5@vku.udn.vn>
2020-11-20 23:06:38 +0700

    first commit

---
+++ Hello_Work.py
@@ -0,0 +1,15 @@
+import random
+
+#day là chạy random các chữ ...
+""" dau 3 nháy là chú thích trên nhiều dòng """
+print(random.choice(['a','b','c']))
+a = None
+a=10;
+
+print("chuyển a thành giá trị từ rỗng sang 10 : ",a)
+
+s='ab'
+s2='long'
+l=[s,s2]
+print(l)
+print(type(s))
